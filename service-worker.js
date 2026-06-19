2d8c18e47ee2883b2054dc47bf2f0047d3883e00
tlecalvez2407 <tlecalvez2407@gmail.com>
2026-06-19 10:19:22 +0200

    auto update

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,32 @@
-const CACHE_NAME = "sleep-cycle-v1";
+const CACHE_NAME = "sleep-cycle-v2";
 
 const FILES = [
   "./",
   "./index.html",
+  "./style.css",
   "./app.js",
   "./manifest.json"
 ];
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => {
+        if (k !== CACHE_NAME) return caches.delete(k);
+      }))
+    )
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
